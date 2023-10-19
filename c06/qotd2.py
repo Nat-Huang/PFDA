20a5b481df672a00b64074d8c805736b3ef87018
Natalie Huang <ngh210001@utdallas.edu>
2023-10-19 12:00:15 -0500

    Modifying qotd2's old script

--- c06/qotd2.py
+++ c06/qotd2.py
@@ -1,6 +1,10 @@
+import random 
+
 def main():
     quotesdb = open("quotesdb.txt", 'r')
     quotes_list = list(quotesdb)
+    quotesdb.close()
+    index = random.randrange(len(quotes_list))
     print(quotes_list)
 
 if __name__ == "__main__":
