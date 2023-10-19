d7e59b02ca575484d24e586b07a3e9526b536e09
Natalie Huang <ngh210001@utdallas.edu>
2023-10-19 12:01:57 -0500

    Add index

--- c06/qotd2.py
+++ c06/qotd2.py
@@ -5,7 +5,7 @@ def main():
     quotes_list = list(quotesdb)
     quotesdb.close()
     index = random.randrange(len(quotes_list))
-    print(quotes_list)
+    print(quotes_list[index], end='')
 
 if __name__ == "__main__":
     main()
