722e1a405cea9e4e254c15e65b35c8075475d111
Natalie Huang <ngh210001@utdallas.edu>
2023-10-19 12:15:40 -0500

    Adding script

--- c06/demo_filewrite.py
+++ c06/demo_filewrite.py
@@ -1,5 +1,11 @@
 def main():
+    content = (
+        '"Not all those who wander are lost." - Bilbo Baggins\n'
+        '"All\'s well that ends better." - Hamfast Gamgee\n'
+    )
 
+    with opem("lotr_quotes.txt", "w") as file:
+        file.write(content)
 
 if __name__ == "__main__":
     main()
