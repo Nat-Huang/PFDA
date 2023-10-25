18e6f3ef3242fd75b55ebe67c2de9feb1a0ca436
Natalie Huang <ngh210001@utdallas.edu>
2023-10-25 14:52:06 -0500

    expanding image

--- c07/mk_thumbnail.py
+++ c07/mk_thumbnail.py
@@ -5,7 +5,7 @@ def main():
     with Image.open("still_life_desk.png") as img:
         out_img = img.resize((img.width//10, img.height//10))
         out_img = out_img.convert('RGB')
-        out_img = out_img.rotate(90)
+        out_img = out_img.rotate(90, expand=True)
         out_img.save("thumbnail.jpg")
 
 
