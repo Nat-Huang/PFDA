623de8770395625be3342d565c50b1bd5f2c0cd7
Natalie Huang <ngh210001@utdallas.edu>
2023-10-25 14:56:28 -0500

    Black and White

--- c07/image_manip.py
+++ c07/image_manip.py
@@ -1,13 +1,20 @@
-from PIL import Image, ImageFilter
+from PIL import Image
+
+
+def rgba_to_greyscale(pixel):
+    r, g, b, a = pixel
+    ave_value = (r + g + b) // 3
+    return (ave_value, ave_value, ave_value, a)
 
 
 def main():
     with Image.open("still_life_desk.png") as img:
-        blur_img = img.filter(ImageFilter.GaussianBlur(radius=10))
-        sharp_img = img.filter(ImageFilter.UnsharpMask(percent=200))
-        img.show()
-        blur_img.show()
-        sharp_img.show()
+        out_img = img.copy()
+        for y in range(img.height):
+            for x in range(img.width//2):
+                pix = img.getpixel((x, y))
+                out_img.putpixel((x, y), rgba_to_greyscale(pix))
+        out_img.show()
 
 
 if __name__ == "__main__":
