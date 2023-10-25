4570bd2bef1616666f4ec12056dc616ae34a2609
Natalie Huang <ngh210001@utdallas.edu>
2023-10-25 14:48:47 -0500

    I'm sure I fixed it.......

--- c07/mk_thumbnail.py
+++ c07/mk_thumbnail.py
@@ -4,6 +4,8 @@ from PIL import Image
 def main():
     with Image.open("still_life_desk.png") as img:
         out_img = img.resize((img.width//10, img.height//10))
+        out_img = out_img.convert('RGB')
+        out_img = out_img.rotate(90)
         out_img.save("thumbnail.jpg")
 
 
