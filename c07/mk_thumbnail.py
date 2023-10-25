365301548d6127109fbcf47ec5f715ab6148f275
Natalie Huang <ngh210001@utdallas.edu>
2023-10-25 14:48:05 -0500

    Add jpg modification

--- c07/mk_thumbnail.py
+++ c07/mk_thumbnail.py
@@ -4,7 +4,7 @@ from PIL import Image
 def main():
     with Image.open("still_life_desk.png") as img:
         out_img = img.resize((img.width//10, img.height//10))
-        out_img.save("thumbnail.png")
+        out_img.save("thumbnail.jpg")
 
 
 if __name__ == "__main__":
