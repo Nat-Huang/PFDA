9530ea5e533f348c178c3167c6d15685161bfb60
Natalie Huang <ngh210001@utdallas.edu>
2023-10-25 14:54:23 -0500

    sharp adjust

--- c07/image_manip.py
+++ c07/image_manip.py
@@ -3,9 +3,11 @@ from PIL import Image, ImageFilter
 
 def main():
     with Image.open("still_life_desk.png") as img:
-        blur_img = img.filter(ImageFilter.GaussianBlur())
+        blur_img = img.filter(ImageFilter.GaussianBlur(radius=10))
+        sharp_img = img.filter(ImageFilter.UnsharpMask(percent=200))
         img.show()
         blur_img.show()
+        sharp_img.show()
 
 
 if __name__ == "__main__":
