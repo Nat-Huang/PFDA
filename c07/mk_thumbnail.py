5bb59e867d50ee4c67393ac35be8b80ab3cc6c18
Natalie Huang <ngh210001@utdallas.edu>
2023-10-25 14:44:56 -0500

    Updated mk script

--- c07/mk_thumbnail.py
+++ c07/mk_thumbnail.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 def main():
     with Image.open("still_life_desk.png") as img:
-        out_img = img.resize((320, 180))
+        out_img = img.resize((img.width//10, img.height//10))
         out_img.save("thumbnail.png")
 
 
