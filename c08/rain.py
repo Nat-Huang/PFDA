212219ca29805dba17c4abff0d7cc77e62ccbd01
Natalie Huang <ngh210001@utdallas.edu>
2023-11-01 12:39:02 -0500

    def main

--- c08/rain.py
+++ c08/rain.py
@@ -2,6 +2,9 @@ import pygame
 
 def main():
     pygame.init()
+    pygame.display.set_caption("Rain") # Give our window a title
+    resolution = (800, 600) 
+    screen = pygame.display.set_mode(resolution) # Set the resolution of our screen surface.
 
 if __name__ == "__main__":
     main()
