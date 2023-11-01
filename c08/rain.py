2185fc5ff0bbec35e1eac51880da2ee03ad695eb
Natalie Huang <ngh210001@utdallas.edu>
2023-11-01 12:41:07 -0500

    full thing

--- c08/rain.py
+++ c08/rain.py
@@ -1,10 +1,22 @@
 import pygame
 
+
 def main():
     pygame.init()
-    pygame.display.set_caption("Rain") # Give our window a title
-    resolution = (800, 600) 
-    screen = pygame.display.set_mode(resolution) # Set the resolution of our screen surface.
+    pygame.display.set_caption("Digital Rain")
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
+    running = True
+    while running:
+        # Event Loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # TODO: some game logic
+        # Render & Display 
+        screen.fill('Green')
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
