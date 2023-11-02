d8b41e9a8c83b615f4ea506acb19302779717bd1
Natalie Huang <ngh210001@utdallas.edu>
2023-11-02 12:56:41 -0500

    twinkle

--- c08/rain.py
+++ c08/rain.py
@@ -1,23 +1,39 @@
+import random
 import pygame
+import math
+
 
 class Particle():
 
     def __init__(self, pos=(0, 0), size=15):
         self.pos = pos
         self.size = size
-        self.color = 'Green'
+        self.color = pygame.Color(0, 255, 0)
+        self.age = 0 # in milliseconds
+        self.surface = self.update_surface()
+
+    def update(self, dt):
+        self.age += dt
+        self.alpha = 255 * (math.sin(math.radians(self.age*0.5)) + 1) / 2
+
+    def update_surface(self):
+        surf = pygame.Surface((self.size, self.size))
+        surf.fill(self.color)
+        return surf
+
+    def draw(self, surface):
+        self.surface.set_alpha(self.alpha)
+        surface.blit(self.surface, self.pos)
+
 
 def main():
     pygame.init()
     pygame.display.set_caption("Digital Rain")
+    clock = pygame.time.Clock()
+    dt = 0
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
     particle = Particle()
-    particle2 = Particle(pos=(2, 2))
-    print(particle.pos)
-    print(particle2.pos)
-    particle.pos = (3, 4)
-    print(particle.pos)
     running = True
     while running:
         # Event Loop
@@ -25,14 +41,14 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         # TODO: some game logic
-        # Render & Display 
+        particle.update(dt)
+        # Render & Display
         black = pygame.Color(0, 0, 0)
         screen.fill(black)
-        surf = pygame.Surface((30, 30))
-        green = pygame.Color(0, 255, 0)
-        surf.fill(green)
-        screen.blit(surf, (resolution[0]//2, resolution[1]//2))
+        particle.draw(screen)
         pygame.display.flip()
+        print(particle.age)
+        dt = clock.tick()
     pygame.quit()
 
 
